674050fbf706e0a3ed867b56ded7ae64e85b187f
Gustavo Afonso <gugustavo137@gmail.com>
2020-12-01 04:33:19 -0300

    Adicionando controller de servicos

--- backend/src/controllers/ServiceController.js
+++ backend/src/controllers/ServiceController.js
@@ -12,4 +12,32 @@ getClient().then(onfulfilled => {
 
 module.exports = {
 
+    async read(request, response) {
+        const service = await Service.findById(request.params.id);
+        return response.json(service);
+    },
+
+    async create(request, response) {
+        try {
+            const service = new Service(request.body);
+            await service.save();
+            return response.send(`${service}`);
+        } catch (error) {
+            console.log(error);
+        }    
+    },
+
+    async update(request, response) {
+        try {
+            const service = await Service.findByIdAndUpdate(request.params.id, request.body, { new: true });
+            await service.save();
+            return response.json(`Service ${service.name} updated!`);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async delete(request, response){
+        const service = await Service.findByIdAndDelete(request.params.id);
+        return response.json('Service deleted!');
+    }
 }
