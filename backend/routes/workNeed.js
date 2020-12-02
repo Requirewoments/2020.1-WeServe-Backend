88580d1363bb491b24d27f21f9caedcec935e11f
MaiconMares <maiconmaresunb@gmail.com>
2020-12-01 23:31:12 -0300

    Criando rota Necessidade de Trabalho e separando rotas.

---
+++ backend/routes/workNeed.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const WorkNeedController = require('./../src/controllers/WorkNeedController');
+
+const routes = Router();
+
+routes.get('/work-need', WorkNeedController.firstMessage);
+routes.get('/work-need/:email', WorkNeedController.read);
+routes.post('/work-need', WorkNeedController.create);
+routes.put('/work-need/:id', WorkNeedController.update);
+routes.delete('/work-need/:id', WorkNeedController.delete);
+
+module.exports = routes;
