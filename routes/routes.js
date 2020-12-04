8c793ab7c023b20c39ab1d22b165c18bc6272a85
Renato Britto Araujo <renatobritto@protonmail.com>
2020-12-03 22:52:42 -0300

    awd

---
+++ routes/routes.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const UserController = require('../src/controllers/UserController');
+const ServiceController = require('../src/controllers/ServiceController');
+const ProposalsController = require('./../src/controllers/ProposalController');
+
+const routes = Router();
+
+routes.get('/', UserController.firstMessage);
+routes.post('/user', UserController.create);
+routes.post('/user/auth', UserController.create);
+routes.get('/user/:id', UserController.read);
+routes.put('/user/:id', UserController.update);
+routes.delete('/user/:id', UserController.delete);
+
+routes.get('/service/:id', ServiceController.read);
+routes.get('/service/', ServiceController.index);
+routes.post('/service', ServiceController.create);
+routes.put('/service/:id', ServiceController.update);
+routes.delete('/service/:id', ServiceController.delete);
+
+routes.get('/proposals', ProposalsController.read);
+routes.post('/proposals', ProposalsController.create);
+routes.put('/proposals/:id', ProposalsController.update);
+routes.delete('/proposals/:id', ProposalsController.delete);
+
+module.exports = routes;
